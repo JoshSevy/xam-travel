d350ed8c611371ef27ce0fb3a7b153185e95f1a9
JoshSevy <joshuasevy@outlook.com>
2022-04-10 22:37:08 -0600

    Init

---
+++ TravelRecordApp/Homepage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class Homepage : TabbedPage
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        void ToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}
