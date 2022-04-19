af24ce59d9bfa2514688971a702c9dade2c56ad3
JoshSevy <joshuasevy@outlook.com>
2022-04-18 22:54:55 -0600

    Add register button

--- TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/MainPage.xaml.cs
@@ -17,17 +17,34 @@ namespace TravelRecordApp
 
         void loginButton_Clicked(System.Object sender, System.EventArgs e)
         {
+            // Need to add more form validation for user login
+            // 
             bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
             bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
-            if (isEmailEmpty || isPasswordEmpty)
+            if (isEmailEmpty && isPasswordEmpty)
             {
-                
+                DisplayAlert("No Entry", "Please enter a valid email and password", "Ok");
+            }
+
+            if (isEmailEmpty)
+            {
+                DisplayAlert("No Entry", "Please enter a valid email", "Ok");
+            }
+
+            if (isPasswordEmpty)
+            {
+                DisplayAlert("No Entry", "Please enter a password!","Ok");
             }
             else
             {
                 Navigation.PushAsync(new Homepage());
             }
+        }
+
+        void registerButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Preview", "Coming Soon!", "Ok");
         }
     }
 }
